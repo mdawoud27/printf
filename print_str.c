0e71231368a149d27271846fc3a222c0e0a30607
mdawoud27 <mhmaddawoud20@gmail.com>
2023-04-25 05:39:19 -0700

    update

--- print_str.c
+++ print_str.c
@@ -7,7 +7,6 @@
  */
 int _print_str(char *str)
 {
-<<<<<<< HEAD
 	int len = 0;
 
 	while (*str != '\0')
@@ -18,18 +17,4 @@ int _print_str(char *str)
 	}
 
 	return (len);
-=======
-int len = 0;
-
-while (*str != '\0')
-        {
-           _print_char(*str);
-           str++;
-	   len++;
-        }
-	
-        return (len);
->>>>>>> c778f7c5cc1d950751aa42c0ff7c417e103dfd49
 }
-
-
