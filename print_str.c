a243326b084fb90b0730f76fc7d2efb0085a1dd2
mdawoud27 <mhmaddawoud20@gmail.com>
2023-04-25 05:31:31 -0700

    update files

--- print_str.c
+++ print_str.c
@@ -7,14 +7,14 @@
  */
 int _print_str(char *str)
 {
-        int len = 0;
+	int len = 0;
 
-        while (*str != '\0')
-        {
-                _print_char(*str);
-                str++;
-                len++;
-        }
+	while (*str != '\0')
+	{
+		_print_char(*str);
+		str++;
+		len++;
+	}
 
-        return (len);
+	return (len);
 }
